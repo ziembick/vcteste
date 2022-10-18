c76f7b280d5af76ced6314d0e985b4fef0933aa5
Paulo Ziembick <pauloziembick@gmail.com>
2022-10-18 18:10:16 -0300

    adicionando querySelectors script.js

---
+++ js/script.js
@@ -0,0 +1,4 @@
+const start_btn = document.querySelector(".start_btn button")
+const info_box = document.querySelector(".info_box");
+const exit_btn = info_box.querySelector(".buttons .quit");
+const continue_btn = document.querySelector(".buttons .restart");
