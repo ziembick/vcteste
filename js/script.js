479a74efda48f9081264ec2fd95ed9b00c473d6e
Paulo Ziembick <pauloziembick@gmail.com>
2022-10-18 18:14:45 -0300

    botao clicar inicializar o quiz

--- js/script.js
+++ js/script.js
@@ -1,4 +1,9 @@
 const start_btn = document.querySelector(".start_btn button")
 const info_box = document.querySelector(".info_box");
 const exit_btn = info_box.querySelector(".buttons .quit");
-const continue_btn = document.querySelector(".buttons .restart");
+const continue_btn = info_box.querySelector(".buttons .restart");
+
+// se o botão de começãr o quiz for clicado
+start_btn.onclick = () => {
+    info_box.classList.add("activeInfo");
+}
