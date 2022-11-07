027aa39dab29a123fffa8d36a2df94be58b354b1
Paulo Ziembick <pauloziembick@gmail.com>
2022-11-07 18:15:17 -0300

    botao de continuar

--- js/script.js
+++ js/script.js
@@ -2,12 +2,20 @@ const start_btn = document.querySelector(".start_btn button")
 const info_box = document.querySelector(".info_box");
 const exit_btn = info_box.querySelector(".buttons .quit");
 const continue_btn = info_box.querySelector(".buttons .restart");
+const quiz_box = info_box.querySelector(".quiz_box");
 
 // se o botão de começãr o quiz for clicado
 start_btn.onclick = () => {
     info_box.classList.add("activeInfo"); // show info box
 }
 
+// se o botão de sair for clicado
 exit_btn.onclick = () => {
     info_box.classList.remove("activeInfo"); // hide info box
+}
+
+// se o botão de continuar for clicado
+continue_btn.onclick = () => {
+    info_box.classList.remove("activeInfo"); // hide info box
+    quiz_box.classList.add("activeQuiz"); // mostra quiz box
 }
