74bbe001cafac650ba6b6c75c46b6d9e51ea4d54
Paulo Ziembick <pauloziembick@gmail.com>
2022-11-07 18:56:18 -0300

    adicionando questions.js e arrumando style

---
+++ js/questions.js
@@ -0,0 +1,57 @@
+let questions = [
+    {   
+        numb: 1,
+        question: "What does HTML stand for?",
+        answer: "",
+        options: [
+            "Hyper Text Preprocessor",
+            "Hyper Text Markup Language",
+            "Hyper Text Multiple Language",
+            "Hyper Tool Multi Language"
+        ]
+    },
+    {   
+        numb: 2,
+        question: "What does HTML stand for?",
+        answer: "",
+        options: [
+            "Hyper Text Preprocessor",
+            "Hyper Text Markup Language",
+            "Hyper Text Multiple Language",
+            "Hyper Tool Multi Language"
+        ]
+    },
+    {   
+        numb: 3,
+        question: "What does HTML stand for?",
+        answer: "",
+        options: [
+            "Hyper Text Preprocessor",
+            "Hyper Text Markup Language",
+            "Hyper Text Multiple Language",
+            "Hyper Tool Multi Language"
+        ]
+    },
+    {   
+        numb: 4,
+        question: "What does HTML stand for?",
+        answer: "",
+        options: [
+            "Hyper Text Preprocessor",
+            "Hyper Text Markup Language",
+            "Hyper Text Multiple Language",
+            "Hyper Tool Multi Language"
+        ]
+    },
+    {   
+        numb: 5,
+        question: "What does HTML stand for?",
+        answer: "",
+        options: [
+            "Hyper Text Preprocessor",
+            "Hyper Text Markup Language",
+            "Hyper Text Multiple Language",
+            "Hyper Tool Multi Language"
+        ]
+    },
+]
--- js/script.js
+++ js/script.js
@@ -2,7 +2,7 @@ const start_btn = document.querySelector(".start_btn button")
 const info_box = document.querySelector(".info_box");
 const exit_btn = info_box.querySelector(".buttons .quit");
 const continue_btn = info_box.querySelector(".buttons .restart");
-const quiz_box = info_box.querySelector(".quiz_box");
+const quiz_box = document.querySelector(".quiz_box");
 
 // se o botão de começãr o quiz for clicado
 start_btn.onclick = () => {
@@ -18,4 +18,15 @@ exit_btn.onclick = () => {
 continue_btn.onclick = () => {
     info_box.classList.remove("activeInfo"); // hide info box
     quiz_box.classList.add("activeQuiz"); // mostra quiz box
+    showQuestions();
+}
+
+let que_count = 0;
+
+// getting questions and options from array
+
+function showQuestions () {
+    const que_text = document.querySelector(".que_text");
+    let que_tag = '<span>'+ questions[0].question + '</span>';
+    que_text.innerHTML = que_tag;
 }
