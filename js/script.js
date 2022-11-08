7c38da8b21773fdbbc6c106426ea21b58f53b265
Paulo Ziembick <pauloziembick@gmail.com>
2022-11-08 13:23:50 -0300

    index e script.js

--- js/script.js
+++ js/script.js
@@ -18,15 +18,21 @@ exit_btn.onclick = () => {
 continue_btn.onclick = () => {
     info_box.classList.remove("activeInfo"); // hide info box
     quiz_box.classList.add("activeQuiz"); // mostra quiz box
-    showQuestions();
+    showQuestions(0);
 }
 
 let que_count = 0;
 
 // getting questions and options from array
 
-function showQuestions () {
+function showQuestions (index) {
     const que_text = document.querySelector(".que_text");
-    let que_tag = '<span>'+ questions[0].question + '</span>';
+    const option_list = document.querySelector(".option_list");
+    let que_tag = '<span>'+ questions[index].question + '</span>';
+    let option_tag = '<div class="option">'+ questions[index].options[0] +'<span></span></div>'
+                    + '<div class="option"><span></span></div>'
+                    + '<div class="option"><span></span></div>'
+                    + '<div class="option"><span></span></div>';
     que_text.innerHTML = que_tag;
+    option_list.innerHTML = option_tag;
 }
