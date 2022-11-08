4fe3642f05dda558a2a99c1ba21ff3eb48781c26
Paulo Ziembick <pauloziembick@gmail.com>
2022-11-08 13:54:28 -0300

    script atualizado

--- js/script.js
+++ js/script.js
@@ -19,20 +19,42 @@ continue_btn.onclick = () => {
     info_box.classList.remove("activeInfo"); // hide info box
     quiz_box.classList.add("activeQuiz"); // mostra quiz box
     showQuestions(0);
+    queCounter(1);
 }
 
 let que_count = 0;
+let que_numb = 1;
+
+const next_btn = quiz_box.querySelector(".next_btn");
+
+//se botao de proximo for clicado
+next_btn.onclick = () => {
+    if (que_count < questions.length - 1) {
+        que_count++;
+        que_numb++;
+        showQuestions(que_count);
+        queCounter(que_numb);
+    }else{
+        console.log("Questions completed");
+    }
+    
+}
 
 // getting questions and options from array
-
 function showQuestions (index) {
     const que_text = document.querySelector(".que_text");
     const option_list = document.querySelector(".option_list");
-    let que_tag = '<span>'+ questions[index].question + '</span>';
+    let que_tag = '<span>'+ questions[index].numb + ". " + questions[index].question + '</span>';
     let option_tag = '<div class="option">'+ questions[index].options[0] +'<span></span></div>'
-                    + '<div class="option"><span></span></div>'
-                    + '<div class="option"><span></span></div>'
-                    + '<div class="option"><span></span></div>';
+                    + '<div class="option">'+ questions[index].options[1] + '<span></span></div>'
+                    + '<div class="option">'+ questions[index].options[2] + '<span></span></div>'
+                    + '<div class="option">'+ questions[index].options[3] + '<span></span></div>';
     que_text.innerHTML = que_tag;
     option_list.innerHTML = option_tag;
+}
+
+function queCounter (index) {
+    const bottom_ques_counter = quiz_box.querySelector(".total_que");
+    let totalQuesCountTag = '<span><p>' + index + '</p>de<p>'+ questions.length + '</p>Perguntas</span>';
+    bottom_ques_counter.innerHTML = totalQuesCountTag
 }
