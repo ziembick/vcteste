36ebd2c55ec690b24f2d3ea12fcd665b6806df50
Paulo Ziembick <pauloziembick@gmail.com>
2022-11-15 17:36:33 -0300

    adjusting timer

--- js/script.js
+++ js/script.js
@@ -4,6 +4,7 @@ const exit_btn = info_box.querySelector(".buttons .quit");
 const continue_btn = info_box.querySelector(".buttons .restart");
 const quiz_box = document.querySelector(".quiz_box");
 const option_list = document.querySelector(".option_list");
+const timeCount = quiz_box.querySelector(".timer .timer_sec");
 
 // se o botão de começãr o quiz for clicado
 start_btn.onclick = () => {
@@ -21,10 +22,12 @@ continue_btn.onclick = () => {
     quiz_box.classList.add("activeQuiz"); // mostra quiz box
     showQuestions(0);
     queCounter(1);
+    startTimer(15);
 }
 
 let que_count = 0;
 let que_numb = 1;
+let counter;
 
 const next_btn = quiz_box.querySelector(".next_btn");
 
@@ -57,11 +60,7 @@ function showQuestions (index) {
     }
 }
 
-function queCounter (index) {
-    const bottom_ques_counter = quiz_box.querySelector(".total_que");
-    let totalQuesCountTag = '<span><p>' + index + '</p>de<p>'+ questions.length + '</p>Perguntas</span>';
-    bottom_ques_counter.innerHTML = totalQuesCountTag
-}
+
 
 
 let tickIcon = '<div class="icon tick"><i class="fas fa-check"></i></div>';
@@ -94,4 +93,22 @@ function optionSelected (answer){
         option_list.children[i].classList.add("disabled")
     }
 
+}
+
+function startTimer(time) {
+    counter = setInterval(timer, 1000);
+    function timer(){
+        timeCount.textContent = time
+        time--;
+    }
+}
+
+
+
+
+
+function queCounter (index) {
+    const bottom_ques_counter = quiz_box.querySelector(".total_que");
+    let totalQuesCountTag = '<span><p>' + index + '</p>de<p>'+ questions.length + '</p>Perguntas</span>';
+    bottom_ques_counter.innerHTML = totalQuesCountTag
 }
